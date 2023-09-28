030008671e99eaf0acf0dcf027d9f4b2fce5c766
goldengrape <github@goldengrape.org>
2023-09-28 11:58:24 -0700

    增加demo

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# retinal_vessel_segment
+# 视网膜血管分割
+
+## 说明
+----            
+
+算法基于以下论文：
+> Li W, Xiao Y, Hu H, Zhu C, Wang H, Liu Z, Sangaiah AK. Retinal Vessel Segmentation Based on B-COSFIRE Filters in Fundus Images. Front Public Health. 2022 Sep 9;10:914973. doi: 10.3389/fpubh.2022.914973. PMID: 36159307; PMCID: PMC9500397.
+[https://www.frontiersin.org/articles/10.3389/fpubh.2022.914973/full](https://www.frontiersin.org/articles/10.3389/fpubh.2022.914973/full)
+            
+算法流程：
+* 使用对比度受限的自适应直方图均衡（CLAHE）进行对比度增强。
+* 对原始RGB图像的CIELab版本的亮度平面进行阈值处理，以产生一个掩码。
+* 应用B-COSFIRE滤波器和形态学滤波器来检测血管并去除噪音。
+* 使用二值阈值进行血管分割。
+* 通过后处理消除未连接的非血管像素，以获得最终的分割图。
+            
+本程序试图仿照论文中的算法，但是由于作者没有公开代码，因此本程序的实现与论文中的算法存在差距。（比论文示例差好多啊😂）
+
+----
+
+## 运行
+
+浏览器打开[https://retinalvessel.streamlit.app/](https://retinalvessel.streamlit.app/) 上传视网膜照片即可运行。可以手动调整各个参数，以观察参数对结果的影响。
--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,6 @@ with st.sidebar:
     post_processing=st.container()
     st.markdown("---")
     resize=st.container()
-    reset_button=st.button('重置参数')
 
 reset_params={
     "fov_threshold":0.1,
@@ -78,25 +77,42 @@ resize_size=resize.number_input(
 st.markdown('''
 ### 视网膜血管分割
 
+----            
+
 算法基于以下论文：
 > Li W, Xiao Y, Hu H, Zhu C, Wang H, Liu Z, Sangaiah AK. Retinal Vessel Segmentation Based on B-COSFIRE Filters in Fundus Images. Front Public Health. 2022 Sep 9;10:914973. doi: 10.3389/fpubh.2022.914973. PMID: 36159307; PMCID: PMC9500397.
 [https://www.frontiersin.org/articles/10.3389/fpubh.2022.914973/full](https://www.frontiersin.org/articles/10.3389/fpubh.2022.914973/full)
+            
+算法流程：
+* 使用对比度受限的自适应直方图均衡（CLAHE）进行对比度增强。
+* 对原始RGB图像的CIELab版本的亮度平面进行阈值处理，以产生一个掩码。
+* 应用B-COSFIRE滤波器和形态学滤波器来检测血管并去除噪音。
+* 使用二值阈值进行血管分割。
+* 通过后处理消除未连接的非血管像素，以获得最终的分割图。
+            
+本程序试图仿照论文中的算法，但是由于作者没有公开代码，因此本程序的实现与论文中的算法存在差距。（比论文示例差好多啊😂）
+
+----
+
 ''')
 # 上传图片
-uploaded_file = st.file_uploader("上传视网膜图片", type=['png', 'jpg', 'jpeg','tif'], accept_multiple_files=False)
+uploaded_file = st.file_uploader("请上传视网膜图片", type=['png', 'jpg', 'jpeg','tif'], accept_multiple_files=False)
 
 if uploaded_file is not None:
     original_image  = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
+else:
+    original_image = cv2.imread('demo.jpg')
+    st.markdown("结果示例")
 
-    imagelist, original_size = extract_vessle(original_image,fov_threshold,clahe_clip_limit,clahe_grid_size,DoG_size,DoG_sigma,blurring_sigma,blurring_alpha,blurring_rho_sample,shifting_phi_sample,geometric_mean_sigma,geometric_mean_t,structuring_element_size,binary_threshold,post_processing_gap,resize_size)
+imagelist, original_size = extract_vessle(original_image,fov_threshold,clahe_clip_limit,clahe_grid_size,DoG_size,DoG_sigma,blurring_sigma,blurring_alpha,blurring_rho_sample,shifting_phi_sample,geometric_mean_sigma,geometric_mean_t,structuring_element_size,binary_threshold,post_processing_gap,resize_size)
 
 
-    retina=cv2.resize(imagelist[0],(original_size[1],original_size[0]))
-    vessel=cv2.resize(imagelist[-2],(original_size[1],original_size[0]))
-    combined=cv2.resize(imagelist[-1],(original_size[1],original_size[0]))
-    col1,col2,col3=st.columns(3)
+retina=cv2.resize(imagelist[0],(original_size[1],original_size[0]))
+vessel=cv2.resize(imagelist[-2],(original_size[1],original_size[0]))
+combined=cv2.resize(imagelist[-1],(original_size[1],original_size[0]))
+col1,col2,col3=st.columns(3)
 
-    col1.image(retina,caption='视网膜图像')
-    col2.image(vessel,caption='血管图像')
-    col3.image(combined,caption='标记图像')
+col1.image(retina,caption='视网膜图像')
+col2.image(vessel,caption='血管图像')
+col3.image(combined,caption='标记图像')
 
